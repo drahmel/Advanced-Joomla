40b06cdd90ec8c6b5385a3b52790eb10d46bb3c8
Dan Rahmel <drahmel7@gmail.com>
2013-12-27 08:46:55 -0800

    DJR:Added dynamic columns

--- chapters/03_DynamicTemplate/aj_dynamic/index.php
+++ chapters/03_DynamicTemplate/aj_dynamic/index.php
@@ -41,22 +41,44 @@ $menu_view = $app->getMenu()->getActive()->query['view'];
 // With so many different template position standards, we'll create an array here of
 // all of the possible position names from the template configuration
 
+$panels = array();
 // Navigation that runs the width of the window
 $temp = $this->params->get('fullnav');
-$fullNav = explode(',', !empty($temp)	?	$temp	:	'banner,fullnav');
+$panels['fullNav'] = explode(',', !empty($temp)	?	$temp	:	'banner,fullnav');
 
 $temp = $this->params->get('leftcol');
-$leftCol = explode(',', !empty($temp)	?	$temp	:	'leftcol,leftslab,position-8,position-4,position-5,login,atomic-sidebar');
+$panels['leftCol'] = explode(',', !empty($temp)	?	$temp	:	'leftcol,leftslab,position-8,position-4,position-5,login,atomic-sidebar');
 
 $temp = $this->params->get('centernav');
-$centerNav = explode(',', !empty($temp)	?	$temp	:	'centernav,breadcrumbs,position-0,position-1,position-6,atomic-topmenu');
+$panels['centerNav'] = explode(',', !empty($temp)	?	$temp	:	'centernav,breadcrumbs,position-0,position-1,position-6,atomic-topmenu');
 
 $temp = $this->params->get('centercol');
-$centerCol = explode(',', !empty($temp)	?	$temp	:	'centercol-top,position-3,MESSAGE,COMPONENT,centercol-bottom');
+$panels['centerCol'] = explode(',', !empty($temp)	?	$temp	:	'centercol-top,position-3,MESSAGE,COMPONENT,centercol-bottom');
 
 $temp = $this->params->get('rightcol');
-$rightCol = explode(',', !empty($temp)	?	$temp	:	'position-7,rightcol');
+$panels['rightCol'] = explode(',', !empty($temp)	?	$temp	:	'position-7,rightcol');
 
+foreach($panels as $panelKey => $panel) {
+	foreach($panel as $positionKey => $position) {
+		$position = trim($position);
+		if(!$this->countModules($position)) {
+			unset($panels[$panelKey][$positionKey]);
+		}
+	}
+}
+//print_r($panels);
+$centerSpan = 12;
+$leftClass = "hideCol";
+$rightClass = "hideCol";
+
+if(!empty($panels['leftCol'])) {
+	$centerSpan -= 3;
+	$leftClass = "span3";
+}
+if(!empty($panels['rightCol'])) {
+	$centerSpan -= 3;
+	$rightClass = "span3";
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -114,6 +136,9 @@ $rightCol = explode(',', !empty($temp)	?	$temp	:	'position-7,rightcol');
 		}
 		label {
 			display: inline;
+		}
+		.hideCol {
+			display: none;
 		}
 		</style>
 	
@@ -179,16 +204,16 @@ $rightCol = explode(',', !empty($temp)	?	$temp	:	'position-7,rightcol');
 			</div>
 			<?php endif; ?>
 			<div class="row-fluid">
-				<div id="leftslab" class="span3">
-					<?php foreach($leftCol as $leftPosition): ?>
-						<jdoc:include type="modules" name="<?php echo $leftPosition ?>" style="sidebar" />
-					<?php endforeach; ?>
-				</div>
-				<div id="centerslab" class="span6">
-					<div class="span12">
-						<?php foreach($centerNav as $centerNavPosition): ?>
-							<jdoc:include type="modules" name="<?php echo $centerNavPosition ?>" style="sidebar" />
+				<?php if(!empty($panels['leftCol'])): ?>
+					<div id="leftslab" class="<?php echo $leftClass ?>">
+						<?php foreach($panels['leftCol'] as $leftPosition): ?>
+							<jdoc:include type="modules" name="<?php echo $leftPosition ?>" style="sidebar" />
 						<?php endforeach; ?>
+					</div>
+				<?php endif; ?>
+				<div id="centerslab" class="<?php echo "span".$centerSpan ?>">
+					<div class="span12">
+						&nbsp;
 					</div>
 					<div id="breadcrumbs">
 						<jdoc:include type="modules" name="position-2" />
@@ -204,7 +229,7 @@ $rightCol = explode(',', !empty($temp)	?	$temp	:	'position-7,rightcol');
 					</div><!--end headerholder-->
 					
 					
-					<?php foreach($centerCol as $centerPosition): ?>
+					<?php foreach($panels['centerCol'] as $centerPosition): ?>
 						<jdoc:include type="modules" name="<?php echo $centerPosition ?>" style="sidebar" />
 					<?php endforeach; ?>
 					<div id="contentholder">
@@ -217,8 +242,10 @@ $rightCol = explode(',', !empty($temp)	?	$temp	:	'position-7,rightcol');
 						</div><!--end contentarea-->
 					</div>
 				</div><!--end centerslab-->
-				<div id="rightslab" class="span3">
-					<jdoc:include type="modules" name="position-7" style="sidebar" />
+				<div id="rightslab" class="<?php echo $rightClass ?>">
+					<?php foreach($panels['rightCol'] as $rightPosition): ?>
+						<jdoc:include type="modules" name="<?php echo $rightPosition ?>" style="sidebar" />
+					<?php endforeach; ?>
 				</div>
 			</div>
 		</div><!--end bodydiv-->
